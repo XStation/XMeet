33a9a1387179a0f997ed3529c7ea444c005a1cd5
ronalfei <ronalfei@qq.com>
2015-01-20 18:17:46 +0800

    change textarea to input

--- public/js/app/chatApp.js
+++ public/js/app/chatApp.js
@@ -9,7 +9,7 @@ define(["require","jquery","simpleSocket",'handlebars','json3'],
           var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
           return "<div class=\"chat-wrap chat-collapse\">\r\n  <div class=\"chat-expand hide\">\r\n    <div class=\"chat-head\">\r\n      <span class=\"title chat-title\">群聊("
             + escapeExpression(((helper = (helper = helpers.payload || (depth0 != null ? depth0.payload : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"payload","hash":{},"data":data}) : helper)))
-            + "人)</span>\r\n      <i class=\"fa fa-close pull-right close-chat\"></i>\r\n    </div>\r\n    <div class=\"chat-main\"> \r\n    </div>\r\n    <div class=\"chat-foot\">\r\n      <textarea class=\"chat-text\" ></textarea>\r\n      <a href=\"#\" class=\"button button-raised button-pill button-inverse button-small pull-right chat-send\">发送(<u>S</u>)</a> \r\n    </div>\r\n  </div>\r\n  <div class=\"chat-collapse-tools\">\r\n    <i class=\"fa fa-wechat\"></i>\r\n    加入聊天室\r\n  </div>\r\n</div>";
+            + "人)</span>\r\n      <i class=\"fa fa-close pull-right close-chat\"></i>\r\n    </div>\r\n    <div class=\"chat-main\"> \r\n    </div>\r\n    <div class=\"chat-foot\">\r\n      <input type=\"text\" class=\"chat-text\" />\r\n      <a href=\"#\" class=\"button button-raised button-pill button-inverse button-small pull-right chat-send\">发送(<u>S</u>)</a> \r\n    </div>\r\n  </div>\r\n  <div class=\"chat-collapse-tools\">\r\n    <i class=\"fa fa-wechat\"></i>\r\n    加入聊天室\r\n  </div>\r\n</div>";
         },"useData":true});
         this["Chat"]["templates"]["msg"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
           var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
@@ -47,7 +47,7 @@ define(["require","jquery","simpleSocket",'handlebars','json3'],
         		   	$('.chat-text').val("");
                 }); 
 				$('.chat-text').keyup(function (e) {
-        		    if(e.keyCode == 13 && e.ctrlKey){ // Ctrl+Enter is pressed
+        		    if(e.keyCode == 13){ // Ctrl+Enter is pressed
         		       ws.send($('.chat-text').val());
         		   	   $('.chat-text').val("");
 					}
