fa7d2bef5d3b214dc631db5ed3ffc1968d0f9105
ronalfei <ronalfei@qq.com>
2015-02-13 16:21:21 +0800

    resize

--- public/js/app/chatApp.js
+++ public/js/app/chatApp.js
@@ -7,7 +7,7 @@ define(["require","jquery","simpleSocket",'handlebars','json3'],
         this["Chat"]["templates"] = this["Chat"]["templates"] || {};
         this["Chat"]["templates"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
           var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-          return "<div class=\"chat-head\">\r\n  <span class=\"title chat-title\">偶遇( "
+          return "<div class=\"chat-head\">\r\n  <span class=\"title chat-title\" style=\"color:white\">偶遇( "
             + escapeExpression(((helper = (helper = helpers.payload || (depth0 != null ? depth0.payload : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"payload","hash":{},"data":data}) : helper)))
             + " 人)</span>\r\n</div>\r\n<div class=\"chat-main\"> \r\n</div>\r\n<div class=\"chat-foot\">\r\n      <textarea class=\"chat-text\" ></textarea>\r\n      <a href=\"#\" class=\"button button-raised button-pill button-inverse button-small pull-right chat-send\">发送</a> \r\n    </div>\r\n</div>";
         },"useData":true});
