10f43232f5a594a99f437d0fb418e8a20c66edb9
ronalfei <ronalfei@qq.com>
2015-02-14 22:37:48 +0800

    fix config from src

--- public/js/XMeet.js
+++ public/js/XMeet.js
@@ -1,12 +1,31 @@
 (function(){
+	XMeet = {};
+	xConfig ={};
+	XMeet.Config=function(arg){
+		if(arg=="" || arg ==undefined){
+			//nothing todo;
+		}else{
+			arr = arg.split('&');
+			for(i in arr){
+				var tmp = arr[i].split('=');
+				xConfig[tmp[0]]=tmp[1];
+			}
+		}
+	}
     var documentReady=window.onload;
     window.onload=function(){
         var fileref = document.createElement('link');
             fileref.setAttribute("rel","stylesheet");
             fileref.setAttribute("type","text/css");
             fileref.setAttribute("href",'http://xpro.im/css/chatWrap.css');
+		var defaultKey = window.location.host+window.location.pathname;		//默认聊天空间为:域名+url, url不带参数
+		if(xConfig.xnest == undefined || xConfig.xnest==""){
+			var xNest = defaultKey;
+		}else{
+			var xNest = window.location.host+":"+xConfig.xnest;
+		}
         var chatConfig={
-          linkKey:window.location.href,//chatRoom ID
+          linkKey: xNest,	//chatRoom ID
         } 
     	var myiframe,chartDiv;
         var chart=[
